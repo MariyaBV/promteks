5868666bc8052cc6d402c5cae483c82a0012924c
Mariia Beliakova <mari.mv2008@gmail.com>
2024-07-10 13:29:14 +0200

    js slider

--- build-tools/src/components/big-slider.js
+++ build-tools/src/components/big-slider.js
@@ -1,22 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     var searchSections = Array.from(document.querySelectorAll('.block-slider'));
-    console.log("searchSections", searchSections);
 
     searchSections.forEach(searchSection => {
         const swiperElement = searchSection.querySelector('.swiper');
-        console.log("swiper", swiperElement);
 
         if (swiperElement) {
             const swiperID = swiperElement.getAttribute('id');
-            console.log("swiperID", swiperID);
 
             if (swiperID) {
                 const sliderNextBtn = searchSection.querySelector('#next_' + swiperID.replace('slider_swiper', ''));
-                console.log("sliderNextBtn", sliderNextBtn);
                 const sliderPrevBtn = searchSection.querySelector('#prev_' + swiperID.replace('slider_swiper', ''));
-                console.log("sliderPrevBtn", sliderPrevBtn);
                 const sliderPagination = searchSection.querySelector('#pagination_' + swiperID.replace('slider_swiper', ''));
-                console.log("sliderPagination", sliderPagination);
 
                 const swiper_block_sliders = new Swiper("#" + swiperID, {
                     direction: 'horizontal',
