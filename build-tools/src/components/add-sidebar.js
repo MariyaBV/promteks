0b283eeed1104ef2984b4ce806c7607988acc719
Mariia Beliakova <mari.mv2008@gmail.com>
2024-08-12 16:52:21 +0200

    pixel perfect

--- build-tools/src/components/add-sidebar.js
+++ build-tools/src/components/add-sidebar.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
     $('.category-list li').has('ul.children').addClass('has-children');
-    $('.category-list li.has-children > a').addClass('icon-Down-3');
+    $('.category-list li.has-children:not(.expanded) > a').addClass('icon-Down-3');
 
     $('.category-list li.has-children > a').on('click', function (e) {
         e.preventDefault();
@@ -17,43 +17,6 @@ jQuery(document).ready(function ($) {
             children.slideDown();
         }
     });
-
-    // Применение стиля к ранее выбранному дочернему элементу или родителю - если нет дочерних
-    // var selectedCategory = localStorage.getItem('selectedCategory');
-    // if (selectedCategory) {
-    //     var selectedElement = $('#' + selectedCategory);
-    //     selectedElement.addClass('selected');
-    //     selectedElement.parents('li').addClass('expanded').children('ul.children').show();
-    //     if (selectedElement.parents('li').find('ul.children').length !== 0) {
-    //         var parentSelectedElement = selectedElement.parent().parent();
-    //         parentSelectedElement.addClass('icon-Vector-9');
-    //         var selectedElementLink = parentSelectedElement.find('a');
-    //         console.log(selectedElementLink);
-    //         selectedElementLink.removeClass('icon-Down-3');
-    //     }
-    // }
-
-    // Добавление стиля при выборе элемента
-    // $('.category-list li a').on('click', function (e) {
-    //     var liElement = $(this).parent();
-    //     if (liElement.find('ul.children').length === 0) {
-    //         // Если у элемента нет дочерних элементов, применяем стиль к нему
-    //         $('.category-list li').removeClass('selected');
-    //         liElement.addClass('selected');
-    //         localStorage.setItem('selectedCategory', liElement.attr('id'));
-    //     } else {
-    //         // Если у элемента есть дочерние элементы, ничего не делаем
-    //         e.preventDefault();
-    //     }
-    // });
-
-    // Добавление стиля при выборе дочернего элемента
-    // $('.category-list ul.children li a').on('click', function (e) {
-    //     var liElement = $(this).parent();
-    //     $('.category-list li').removeClass('selected');
-    //     liElement.addClass('selected');
-    //     localStorage.setItem('selectedCategory', liElement.attr('id'));
-    // });
 });
 
 
